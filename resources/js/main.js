3f14a4ace20364dd4e5caa88c296c839e287019e
Akash Panchal <akashp1712@gmail.com>
2020-04-04 11:37:53 +0000

    Restructuring and cleaning
    Signed-off-by: Akash Panchal <akashp1712@gmail.com>

--- resources/js/main.js
+++ resources/js/main.js
@@ -23,10 +23,14 @@ $(function(){
 	/************
 	   VARIABLES
 	 ************/
-	
-	var params;
-	var modules = [];
-	var stylelang = '';
+
+	var dataPreProcessing;
+	$.getJSON( "resources/data/pre-processing.json", function(data) {
+        dataPreProcessing = data;
+    });
+
+	var codeList = [];
+	var pipelineData = {};
 
 	// vars to hold files(string) and folders (dictionary of a single key:value(list) pair)
 	var setup_list = ['setup.py', {'sample': ['__init__.py','main.py']}, {'test': ['test_basic.py']}];
@@ -78,112 +82,67 @@ $(function(){
 	
 	function update(){
 		updateModules();
-		updateUrls();
-		updateTree();
+		updateCode();
+		updatePipeline();
 	}
 	
 	function updateModules(){
-		modules = [];
-		$('input').each(function(){
-			if ($(this).is(':checked'))
-				modules.push($(this).val());
-		});
+		codeList = [];
+		pipelineData = {};
+
+       pipelineData["pre-processing"] = [];
+		$("#pre-processing").find("input:checked").each(function() {
+            codeList.push(dataPreProcessing[$(this).val()]);
+            pipelineData["pre-processing"].push($(this).val());
+        });
+
+        $("#models").find("input:checked").each(function() {
+            // TODO: do same as above
+        });
+
+        $("#post-processing").find("input:checked").each(function() {
+            // TODO: do same as above
+        });
 	}
 
-	function updateUrls(){
-		var modeParam = '';
-		
-		if (stylelang != ''){
-			modeParam = 'mode=' + stylelang + '&';
+	function updateCode() {
+
+        var codeFull = [];
+		for (var i = 0, code; code = codeList[i++];){
+		    if (i != 0) { codeFull = codeFull.concat(["",""]); }
+		    codeFull = codeFull.concat(code);
 		}
 
-		params = '';
-		
-		for (var i = 0, curModule; curModule = modules[i++];){
-			params += curModule + '&';
-		}
-		
-		params = params.substring(0, params.length - 1);
+        displayData(codeFull);
 
-		$('#preview-url').val(config.baseUrl + 'print&' + modeParam + params);
-		$('#download-url').val(config.baseUrl + modeParam + params);	
-		
-		$('#preview-link').attr('href', config.baseUrl + 'print&' + modeParam + params);
-		$('#download-link').attr('href', config.baseUrl + modeParam + params);	
-	}
+         function displayData(codeLines) {
+            // Display the data
+            var codeElem = "";
 
-	function updateTree(){
+            codeLines.forEach(function(value) {
+                codeElem = codeElem + value + "<br>";
+            });
 
-        var treeChildList = [];
-        var treeChildList = getListOfRequiredFiles();
-
-        var parentUL=document.createElement('ul');
-        parentUL.className = "tree";
-        generateChildTree(parentUL, [{'pythonizr.py': treeChildList}])
-
-        var x = document.getElementsByClassName("tree-block");
-        x[0].innerHTML = '';
-        x[0].appendChild(parentUL);
-
-	}
-
-    /**************************
-        HELPERS FOR TREE-VIEW
-    **************************/
-
-    // Get the list of files//dirs required to print in TreeView
-    function getListOfRequiredFiles() {
-
-        var fileList = [];
-        for (var i=0, curModule; curModule=modules[i++];) {
-            fileName = preModules[curModule];
-            if (typeof fileName === 'string') {
-                // variable is string
-                fileList.push(fileName);
-            } else if (!(typeof fileName === 'undefined')) {
-                // variable is list
-                for (var k = 0, file; file = fileName[k++];) {
-                    fileList.push(file);
-                }
-            }
-        }
-        return fileList;
+            $("#code").html(codeElem);
+            $('pre code').each(function(i, e) {hljs.highlightBlock(e);}); // highlight the code
+	    }
     }
 
-    // Recursive function to generate DOM for files and folders from the list
-    function generateChildTree(ulTemp, setupModules) {
+    function updatePipeline() {
 
-        // eliminate the undefined types
-        if (!(typeof setupModules === 'undefined')) {
+        var dataPipe = document.getElementById("data-pipe");
+        dataPipe.innerHTML = '';
 
-            for (var j= 0, listValue; listValue = setupModules[j++];) {
+        var header = document.createElement('h3');
+        header.innerHTML="Data";
+        dataPipe.appendChild(header);
 
-               child = listValue;
-               if (typeof child === 'string') {
-                  // child is string, append on same level
-                    var liSetupFile=document.createElement('li');
-                    liSetupFile.innerHTML=child;
-                    ulTemp.appendChild(liSetupFile);
+        pipelineData["pre-processing"].forEach(function(value) {
+            var p = document.createElement('p');
+            p.innerHTML = value;
+            dataPipe.appendChild(p);
+		});
 
-               } else {
-               // child is list of dictionaries, create new folder for each with key as folder name and value of list as files
-
-                   for (var key in child) {
-                       var liFolder = document.createElement('li');
-                       liFolder.innerHTML = key; // root folder
-
-                       var ulInside = document.createElement('ul');
-                       ulInside.className = 'tree';
-
-                       // recursive call for list in the directory
-                       generateChildTree(ulInside, child[key]);
-
-                       liFolder.appendChild(ulInside);
-                       ulTemp.appendChild(liFolder);
-                    }
-                }
-            }
-        }
     }
 
 	/***********
@@ -214,6 +173,5 @@ $(function(){
 		$('#hidden-section').fadeIn(0);
 
 	update();
-	
 
 });
