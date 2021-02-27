815f44d1ea6af3e6c4e9608fa493a0d0b1d3cfb3
Akash Panchal <akashp1712@gmail.com>
2021-02-27 22:15:07 +0530

    Ran Prettier on json and js file

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,261 +1,281 @@
-$(function(){
-
-    /**************
+$(function () {
+  /**************
      DATA LOADING
     **************/
 
-    var urlPreProcessing = "resources/data/pre-processing.json";
-    var urlModels = "resources/data/models.json";
-    var urlPostProcessing = "resources/data/post-processing.json";
+  var urlPreProcessing = "resources/data/pre-processing.json";
+  var urlModels = "resources/data/models.json";
+  var urlPostProcessing = "resources/data/post-processing.json";
 
-    var dataPreProcessing, dataModels, dataPostProcessing;
-    $.when(
-        $.getJSON(urlPreProcessing, function(data) {
-            dataPreProcessing = data;
-        }),
-        $.getJSON(urlModels, function(data) {
-            dataModels = data;
-        }),
-        $.getJSON(urlPostProcessing, function(data) {
-            dataPostProcessing = data;
-        })
-    ).then(function() {
-        if (dataPreProcessing && dataModels) {
-            loadMain(); // load main script
-            $('#all-content').addClass('loaded'); // Make the main content visible
-        }
-    });
+  var dataPreProcessing, dataModels, dataPostProcessing;
+  $.when(
+    $.getJSON(urlPreProcessing, function (data) {
+      dataPreProcessing = data;
+    }),
+    $.getJSON(urlModels, function (data) {
+      dataModels = data;
+    }),
+    $.getJSON(urlPostProcessing, function (data) {
+      dataPostProcessing = data;
+    })
+  ).then(function () {
+    if (dataPreProcessing && dataModels) {
+      loadMain(); // load main script
+      $("#all-content").addClass("loaded"); // Make the main content visible
+    }
+  });
 
-    function loadMain() {
-        /************
+  function loadMain() {
+    /************
             CONFIG
          ************/
-        var config = {
-            defaultModules:{
-                regression: [
-                    'dataLoading',
-                    'trainTestSplit',
-                    'linearReg',
-                    'mae'
-                ],
-                classification: [
-                    'dataLoading',
-                    'trainTestSplit',
-                    'svm',
-                    'accuracyScore'
-                ]
-            },
-        };
+    var config = {
+      defaultModules: {
+        regression: ["dataLoading", "trainTestSplit", "linearReg", "mae"],
+        classification: [
+          "dataLoading",
+          "trainTestSplit",
+          "svm",
+          "accuracyScore",
+        ],
+      },
+    };
 
-        /************
+    /************
            VARIABLES
          ************/
 
-        var codeList = [];
-        var pipelineData = {};
+    var codeList = [];
+    var pipelineData = {};
 
-        /**********
+    /**********
            EVENTS
          **********/
 
-        $('input').click(function(){
-            update();
-        });
+    $("input").click(function () {
+      update();
+    });
 
-        $('#preconfig-regression').click(function(){
-            fillDefaultModules('regression');
-            loadRegressionData();
-        });
+    $("#preconfig-regression").click(function () {
+      fillDefaultModules("regression");
+      loadRegressionData();
+    });
 
-        $('#preconfig-classification').click(function(){
-            fillDefaultModules('classification');
-            loadClassificationData();
-        });
+    $("#preconfig-classification").click(function () {
+      fillDefaultModules("classification");
+      loadClassificationData();
+    });
 
-        /*********
+    /*********
            LOGIC
          *********/
 
-        function fillDefaultModules(type){
-            $('input').attr('checked', false);
+    function fillDefaultModules(type) {
+      $("input").attr("checked", false);
 
-            for (var i = 0, curModule; curModule = config.defaultModules[type][i++];){
-                $('input[value=' + curModule +']').attr('checked', true);
-            };
+      for (
+        var i = 0, curModule;
+        (curModule = config.defaultModules[type][i++]);
 
-            update();
-            $('#hidden-section').fadeIn('slow');
-            highlightBadge(); // Add badge for copy to clipboard
+      ) {
+        $("input[value=" + curModule + "]").attr("checked", true);
+      }
+
+      update();
+      $("#hidden-section").fadeIn("slow");
+      highlightBadge(); // Add badge for copy to clipboard
+    }
+
+    function loadRegressionData() {
+      $("#models").find("h3").html("Model (Regression)");
+      $("#regression-models").show();
+      $("#classification-models").hide();
+
+      $("#post-processing").find("h3").html("Post-processing (Regression)");
+      $("#regression-techniques").show();
+      $("#classification-techniques").hide();
+    }
+
+    function loadClassificationData() {
+      $("#models").find("h3").html("Model (Classification)");
+      $("#regression-models").hide();
+      $("#classification-models").show();
+
+      $("#post-processing").find("h3").html("Post-processing (Classification)");
+      $("#regression-techniques").hide();
+      $("#classification-techniques").show();
+    }
+
+    function update() {
+      updateModules();
+      updateCode();
+      updatePipeline();
+    }
+
+    function updateModules() {
+      importList = [];
+      globalList = [];
+      codeList = [];
+      pipelineData = {};
+
+      /*Add components from each module*/
+      pipelineData["pre-processing"] = [dataPreProcessing["name"]];
+      $("#pre-processing")
+        .find("input:checked")
+        .each(function () {
+          var value = $(this).val();
+          if (value != "none") {
+            importList.push(dataPreProcessing["data"][value]["imports"]); // Add imports
+            globalList.push(dataPreProcessing["data"][value]["global"]); // Add global variables
+            codeList.push(dataPreProcessing["data"][value]["code"]); // Add code
+            pipelineData["pre-processing"].push(
+              dataPreProcessing["data"][value]["name"]
+            ); // Add pipeline component
+          }
+        });
+
+      pipelineData["models"] = [dataModels["name"]];
+      $("#models")
+        .find("input:checked")
+        .each(function () {
+          var value = $(this).val();
+          importList.push(dataModels["data"][value]["imports"]); // Add imports
+          globalList.push(dataModels["data"][value]["global"]); // Add global variables
+          codeList.push(dataModels["data"][value]["code"]); // Add code
+          pipelineData["models"].push(dataModels["data"][value]["name"]); // Add pipeline component
+        });
+
+      pipelineData["post-processing"] = [dataPostProcessing["name"]];
+      $("#post-processing")
+        .find("input:checked")
+        .each(function () {
+          var value = $(this).val();
+          importList.push(dataPostProcessing["data"][value]["imports"]); // Add imports
+          codeList.push(dataPostProcessing["data"][value]["code"]); // Add code
+          pipelineData["post-processing"].push(
+            dataPostProcessing["data"][value]["name"]
+          ); // Add pipeline component
+        });
+    }
+
+    function updateCode() {
+      // Include all the imports
+      var codeFull = [];
+      for (var i = 0, code; (code = importList[i++]); ) {
+        codeFull = codeFull.concat(code);
+      }
+
+      codeFull = codeFull.concat([""]);
+      // Include all the global variables
+      for (var i = 0, code; (code = globalList[i++]); ) {
+        codeFull = codeFull.concat(code);
+      }
+
+      // Include all the code
+      for (var i = 0, code; (code = codeList[i++]); ) {
+        codeFull = codeFull.concat([""]);
+        codeFull = codeFull.concat(code);
+      }
+      displayData(codeFull);
+
+      // Display the list of code lines
+      function displayData(codeLines) {
+        var codeElem = "";
+        codeLines.forEach(function (value) {
+          codeElem = codeElem + value + "<br>";
+        });
+        $("#code").html(codeElem);
+        $("pre code").each(function (i, e) {
+          hljs.highlightBlock(e);
+        }); // highlight the code
+      }
+    }
+
+    function highlightBadge() {
+      var options = {
+        // optional
+        copyIconClass: "fas fa-copy",
+        checkIconClass: "fas fa-check text-success",
+      };
+      window.highlightJsBadge(options);
+    }
+
+    function updatePipeline() {
+      var x = document.getElementsByClassName("container__sources");
+      x[0].innerHTML = "";
+      var keys = Object.keys(pipelineData);
+      var needLine = false;
+
+      for (var i = 0, pipeKey; (pipeKey = keys[i++]); ) {
+        var dataPipe = document.createElement("div"); // Create div for each block
+        dataPipe.className = "step";
+        dataPipe.innerHTML = "";
+        pipeData = pipelineData[pipeKey];
+
+        if (pipeData.length > 1) {
+          // Pipe has some function (1 is header)
+          var header = document.createElement("h3"); // Add header
+          header.innerHTML = pipeData[0];
+          dataPipe.appendChild(header);
+
+          for (var j = 1, value; (value = pipeData[j++]); ) {
+            // Add each added element
+            var p = document.createElement("p");
+            p.innerHTML = value;
+            dataPipe.appendChild(p);
+          }
+
+          // Append the animated line before every pipe except first
+          if (needLine == true) {
+            x[0].appendChild(getAnimatedLine());
+          }
+          x[0].appendChild(dataPipe);
+          needLine = true;
         }
+      }
 
-        function loadRegressionData() {
-            $("#models").find("h3").html("Model (Regression)");
-            $('#regression-models').show();
-            $('#classification-models').hide();
+      function getAnimatedLine() {
+        var svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+        svg.setAttribute("viewBox", "0 0 100 80");
 
-            $("#post-processing").find("h3").html("Post-processing (Regression)");
-            $('#regression-techniques').show();
-            $('#classification-techniques').hide();
-        }
+        var line = document.createElementNS(
+          "http://www.w3.org/2000/svg",
+          "line"
+        );
+        line.setAttribute("x1", "5");
+        line.setAttribute("x2", "100");
+        line.setAttribute("y1", "70");
+        line.setAttribute("y2", "70");
 
-        function loadClassificationData() {
-            $("#models").find("h3").html("Model (Classification)");
-            $('#regression-models').hide();
-            $('#classification-models').show();
+        svg.appendChild(line);
+        return svg;
+      }
+    }
 
-            $("#post-processing").find("h3").html("Post-processing (Classification)");
-            $('#regression-techniques').hide();
-            $('#classification-techniques').show();
-        }
-
-        function update(){
-            updateModules();
-            updateCode();
-            updatePipeline();
-        }
-
-        function updateModules(){
-            importList = [];
-            globalList = []
-            codeList = [];
-            pipelineData = {};
-
-            /*Add components from each module*/
-            pipelineData["pre-processing"] = [dataPreProcessing["name"]];
-            $("#pre-processing").find("input:checked").each(function() {
-                var value = $(this).val();
-                if (value != "none") {
-                    importList.push(dataPreProcessing["data"][value]["imports"]); // Add imports
-                    globalList.push(dataPreProcessing["data"][value]["global"]); // Add global variables
-                    codeList.push(dataPreProcessing["data"][value]["code"]); // Add code
-                    pipelineData["pre-processing"].push(dataPreProcessing["data"][value]["name"]); // Add pipeline component
-                }
-            });
-
-            pipelineData["models"] = [dataModels["name"]];
-            $("#models").find("input:checked").each(function() {
-                var value = $(this).val();
-                importList.push(dataModels["data"][value]["imports"]); // Add imports
-                globalList.push(dataModels["data"][value]["global"]); // Add global variables
-                codeList.push(dataModels["data"][value]["code"]); // Add code
-                pipelineData["models"].push(dataModels["data"][value]["name"]); // Add pipeline component
-            });
-
-            pipelineData["post-processing"] = [dataPostProcessing["name"]];
-            $("#post-processing").find("input:checked").each(function() {
-                var value = $(this).val();
-                importList.push(dataPostProcessing["data"][value]["imports"]); // Add imports
-                codeList.push(dataPostProcessing["data"][value]["code"]); // Add code
-                pipelineData["post-processing"].push(dataPostProcessing["data"][value]["name"]); // Add pipeline component
-            });
-        }
-
-        function updateCode() {
-
-            // Include all the imports
-            var codeFull = [];
-            for (var i = 0, code; code = importList[i++];){codeFull = codeFull.concat(code);}
-
-            codeFull = codeFull.concat([""]);
-            // Include all the global variables
-            for (var i = 0, code; code = globalList[i++];){codeFull = codeFull.concat(code);}
-
-            // Include all the code
-            for (var i = 0, code; code = codeList[i++];){
-                codeFull = codeFull.concat([""]);
-                codeFull = codeFull.concat(code);
-            }
-            displayData(codeFull);
-
-            // Display the list of code lines
-            function displayData(codeLines) {
-                var codeElem = "";
-                codeLines.forEach(function(value) {codeElem = codeElem + value + "<br>";});
-                $("#code").html(codeElem);
-                $('pre code').each(function(i, e) {hljs.highlightBlock(e);}); // highlight the code
-            }
-        }
-
-        function highlightBadge() {
-            var options = {   // optional
-               copyIconClass: "fas fa-copy",
-               checkIconClass: "fas fa-check text-success"
-            };
-            window.highlightJsBadge(options);
-        }
-
-        function updatePipeline() {
-            var x = document.getElementsByClassName("container__sources");
-            x[0].innerHTML = '';
-            var keys = Object.keys(pipelineData);
-            var needLine = false;
-
-            for (var i = 0, pipeKey; pipeKey = keys[i++];){
-                var dataPipe=document.createElement('div'); // Create div for each block
-                dataPipe.className = "step";
-                dataPipe.innerHTML = '';
-                pipeData = pipelineData[pipeKey];
-
-                if (pipeData.length > 1) { // Pipe has some function (1 is header)
-                    var header = document.createElement('h3'); // Add header
-                    header.innerHTML = pipeData[0];
-                    dataPipe.appendChild(header);
-
-                    for (var j = 1, value; value = pipeData[j++];) { // Add each added element
-                        var p = document.createElement('p');
-                        p.innerHTML = value;
-                        dataPipe.appendChild(p);
-                    }
-
-                     // Append the animated line before every pipe except first
-                    if (needLine == true) {x[0].appendChild(getAnimatedLine());}
-                    x[0].appendChild(dataPipe);
-                    needLine = true;
-                }
-            }
-
-            function getAnimatedLine() {
-                var svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-                svg.setAttribute('viewBox', "0 0 100 80");
-
-                var line = document.createElementNS('http://www.w3.org/2000/svg','line');
-                line.setAttribute('x1', "5");
-                line.setAttribute('x2', "100");
-                line.setAttribute('y1', "70");
-                line.setAttribute('y2', "70");
-
-                svg.appendChild(line);
-                return svg;
-            }
-        }
-
-        /***********
+    /***********
            HELPERS
          ***********/
 
-        if (!Array.indexOf){
-            Array.prototype.indexOf = function(searchedElement){
-                for (var i = 0; i < this.length; i++){
-                    if (this[i] === searchedElement)
-                        return i;
-                };
-                return -1;
-            };
+    if (!Array.indexOf) {
+      Array.prototype.indexOf = function (searchedElement) {
+        for (var i = 0; i < this.length; i++) {
+          if (this[i] === searchedElement) return i;
         }
+        return -1;
+      };
+    }
 
-        Array.prototype.remove = function(searchedElement){
-            var i = this.indexOf(searchedElement);
-            if (i != -1)
-                this.splice(i, 1);
-        };
+    Array.prototype.remove = function (searchedElement) {
+      var i = this.indexOf(searchedElement);
+      if (i != -1) this.splice(i, 1);
+    };
 
-        /***********
+    /***********
             MAIN
          ***********/
 
-        if ($('input:checked').length > 0)
-            $('#hidden-section').fadeIn(0);
+    if ($("input:checked").length > 0) $("#hidden-section").fadeIn(0);
 
-        update();
-    }
+    update();
+  }
 });
